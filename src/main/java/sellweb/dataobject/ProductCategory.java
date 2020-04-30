16871af1c857b8a80adb70feb64929fd19963e47
lingya <hly19971203@hotmail.com>
2020-04-30 10:46:14 -0700

    completes the DAO of product category

---
+++ src/main/java/sellweb/dataobject/ProductCategory.java
@@ -0,0 +1,43 @@
+package sellweb.dataobject;
+
+import lombok.Data;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+/**
+ * The type Product category.
+ */
+@Entity
+@DynamicUpdate
+@Data
+public class ProductCategory {
+
+    @Id
+    @GeneratedValue
+    private Integer categoryId;
+
+    private String categoryName;
+
+    private Integer categoryType;
+
+    /**
+     * Instantiates a new Product category.
+     */
+    public ProductCategory() {
+    }
+
+    /**
+     * Instantiates a new Product category.
+     *
+     * @param categoryName the category name
+     * @param categoryType the category type
+     */
+    public ProductCategory(String categoryName, Integer categoryType) {
+        this.categoryName = categoryName;
+        this.categoryType = categoryType;
+    }
+}
